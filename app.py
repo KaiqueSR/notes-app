d026ddcb4182d072d8b7d0690ea8ccc03899bada
Kaique Ramos <ramoskaique1324@gmail.com>
2023-02-18 14:58:44 -0300

    model user created and flask-migrate added

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
+import os
+
 from flask import Flask
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+from db import db
+import models
 
 
 def create_app():
     app = Flask(__name__)
+
+    load_dotenv()
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Notes App API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
 
     @app.route("/")
     def hello():
---
+++ models/user.py
@@ -0,0 +1,10 @@
+from db import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.String, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
